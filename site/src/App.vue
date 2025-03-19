0db14250fd0cadb30bfee53678f5e8f74a28c0fa
Tilly Woodfield <22456167+tillywoodfield@users.noreply.github.com>
2025-03-19 10:13:09 +0200

    feat: remove unused code

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <v-app>
-    <v-app-bar v-if="false" app>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </v-app-bar>
     <v-main>
       <router-view/>
     </v-main>
